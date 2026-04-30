49f5481e78c1db7e8b7cb2b8c223f981035ab822
taykas <thaynaschaeffer@gmail.com>
2026-04-30 08:28:27 -0300

    feat: creating project and adding router dom

---
+++ front-end/src/App.jsx
@@ -0,0 +1,16 @@
+import App from './App.jsx'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
